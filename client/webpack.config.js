63fef899dcc782e18c942251ea4e9e2305c5c39a
jairoPonti <jairmponti@gmail.com>
2021-04-29 00:50:00 -0300

    creacion client

---
+++ client/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+      rules: [
+         {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+         }, 
+         {
+             test:/\.css$/,
+             use: ['style-loader', 'css-loader']
+         },
+         {
+            test:/\.png/,
+            type: 'asset/resource'
+        },
+      ]
+  },
+  devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+    //   port: 3000,      Por default abre en 8080, sino lo cambiamos acá
+      open: true
+  }
+};
